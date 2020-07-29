a45563aadd163bdfb4e5dfc2f87799a2970776a0
Bilgehan Gecici <bilgehangecici@gmail.com>
2020-07-29 22:03:23 +0300

    API request fixed and updated.

--- main.js
+++ main.js
@@ -14,23 +14,31 @@ const currencyEl_two = document.getElementById('currency-two');
 const rateEl = document.getElementById('rate');
 
 // swap
-const swap = document.getElementById('swap');
+const swap = document.getElementById('swapBtn');
 
 
 function calculate() {
+
+    // Catch DOM Values.
     const currency_one = currencyEl_one.value;
     const currency_two = currencyEl_two.value;
-   console.log("Currency two" + currency_two);
 
-    fetch(`https://v6.exchangerate-api.com/v6/8aecfbb87d7e13980ca318cc/latest/${currency_one}`)
+    fetch(`https://v6.exchangerate-api.com/v6/8aecfbb87d7e13980ca318cc/latest/USD`) //USD Based API.
         .then(res => res.json())
         .then(data => {
-            
-            console.log(data);
-            const conversionRates = data.conversion_rates[currency_two];
-            rateEl.innerHTML = `1 ${currency_one} = ${conversionRates} ${currency_two}`;
 
-            amountEl_two.value = (amountEl_one.value * conversionRates).toFixed(2);
+            console.log(data);
+
+            const currency1 = data.conversion_rates[currency_one]; // USD BASE
+            const currency2 = data.conversion_rates[currency_two]; // TRY BASE
+
+            const RATE = 1 / currency1; // RATE Calculation
+            const ActualValue = RATE * currency2; // Actual Value.
+            var showedResult = ActualValue.toFixed(2);
+
+            rateEl.innerHTML = `1 ${currency_one} = ${showedResult} ${currency_two}`;
+
+            amountEl_two.value = (amountEl_one.value * ActualValue).toFixed(2);
         });
 }
 
